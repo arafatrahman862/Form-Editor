9ea4e1a4fa7f3334c53e755becdca8348361a90c
xtr <99972602+arafatrahman862@users.noreply.github.com>
2023-11-01 00:22:21 +0600

    input field added

--- src/Components/Cloze.jsx
+++ src/Components/Cloze.jsx
@@ -10,38 +10,44 @@ const Cloze = () => {
     const {
         register,
         handleSubmit,
-       
+
         formState: { errors }
-      } = useForm();
-      const onSubmit = (data) => {
-       
+    } = useForm();
+    const onSubmit = (data) => {
+
         setValue(data)
         console.log(value.sentence);
-      };
-     
+    };
+
     return (
         <div className='border-2 border-blue-400 rounded-lg p-4 mx-8'>
-         <div>
-            <p className='py-2 text-lg'>Question 2</p>
-         <div>
-                <p>Preview*</p>
-              
-                <p className='px-8 py-4 border-2 rounded-lg w-1/2'>{value.sentence}</p>
-            </div>
-            <form onSubmit={handleSubmit(onSubmit)} className='mb-4'>
-                <div className='flex items-center gap-4 '>
-                <div className="form-control w-1/2">
-                    <label className="label">
-                        <span className="label-text">Sentence*</span>
-                    </label>
-                    <input type="text" name="sentence" {...register("sentence")} placeholder="Underline the words here to convert them into blanks" className="input input-bordered " />
-                   
+            <div>
+                <div className='flex justify-between mt-4 items-center'>
+                    <p className='py-2 text-lg'>Question 2</p>
+                    <div >
+                        <p>Points</p>
+                        <input type="number" placeholder="" className="input input-bordered w-[50%] " />
+                    </div>
                 </div>
-                <input  className="btn btn-primary mt-10 " type="submit" value="save" />
-                </div>
-            </form>
+                <div>
+                    <p>Preview*</p>
 
-         </div>
+                    <p className='px-8 py-4 border-2 rounded-lg w-1/2'>{value.sentence}</p>
+                </div>
+                <form onSubmit={handleSubmit(onSubmit)} className='mb-4'>
+                    <div className='flex items-center gap-4 '>
+                        <div className="form-control w-1/2">
+                            <label className="label">
+                                <span className="label-text">Sentence*</span>
+                            </label>
+                            <input type="text" name="sentence" {...register("sentence")} placeholder="Underline the words here to convert them into blanks" className="input input-bordered " />
+
+                        </div>
+                        <input className="btn btn-primary mt-10 " type="submit" value="save" />
+                    </div>
+                </form>
+
+            </div>
         </div>
     );
 };
